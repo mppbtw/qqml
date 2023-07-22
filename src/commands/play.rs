88559495e94f69513ca30d587592b0ca98ee1084
MrPiggyPegasus <charbalone@gmail.com>
2023-07-22 15:26:58 +0100

    things

--- src/commands/play.rs
+++ src/commands/play.rs
@@ -1,3 +1,9 @@
+use crate::utils::{self, print_error};
+
 pub fn play() {
+    if !utils::dotfile_is_valid() {
+        print_error("The ~/.yarr/ directory is not set up; please run yarr init to create it.");
+        return;
+    }
     println!("Play subcommand");
 }
