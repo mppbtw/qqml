5e57186c7cbb28a671af10dc46dbe6ae0e6c9053
mppbtw <mppdevv@skiff.com>
2023-07-22 20:38:52 +0100

    sanity

--- src/qqml/token.rs
+++ src/qqml/token.rs
@@ -1,10 +1,16 @@
-#[derive(Debug)]
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+use crate::cow_map;
+use std::borrow::Cow;
+
+#[derive(Debug, Clone)]
 pub enum Token {
     Illegal,
     Eof,
 
     Ident(String),
     Number(usize),
+    Literal(String),
 
     Equal,
     Comma,
@@ -25,9 +31,23 @@ pub enum Token {
     Multichoice,
     String,
     Calculation,
+    Inputs,
 }
+
 impl PartialEq for Token {
     fn eq(&self, _: &Self) -> bool {
         matches!(self, _)
     }
+}
+
+lazy_static! {
+    pub static ref KEYWORDS: HashMap<String, Token> = {
+        let mut m = HashMap::new();
+        m.insert("ask".into(), Token::Ask);
+        m.insert("multichoice".into(), Token::Multichoice);
+        m.insert("string".into(), Token::String);
+        m.insert("calculation".into(), Token::Calculation);
+        m.insert("inputs".into(), Token::Inputs);
+        m
+    };
 }
