83307e63ebbd2da13902e6d953f95c46cb11ceda
MrPiggyPegasus <charbalone@gmail.com>
2023-07-23 23:11:06 +0100

    added names for tokens

--- qqml_lexer/src/token.rs
+++ qqml_lexer/src/token.rs
@@ -1,5 +1,17 @@
 use lazy_static::lazy_static;
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
+
+lazy_static! {
+    pub static ref KEYWORDS: HashMap<String, Token> = {
+        let mut m = HashMap::new();
+        m.insert("ask".into(), Token::Ask);
+        m.insert("multichoice".into(), Token::Multichoice);
+        m.insert("string".into(), Token::String);
+        m.insert("calculation".into(), Token::Calculation);
+        m.insert("inputs".into(), Token::Inputs);
+        m
+    };
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
@@ -39,14 +51,41 @@ pub enum Token {
     Calculation,
     Inputs,
 }
-lazy_static! {
-    pub static ref KEYWORDS: HashMap<String, Token> = {
-        let mut m = HashMap::new();
-        m.insert("ask".into(), Token::Ask);
-        m.insert("multichoice".into(), Token::Multichoice);
-        m.insert("string".into(), Token::String);
-        m.insert("calculation".into(), Token::Calculation);
-        m.insert("inputs".into(), Token::Inputs);
-        m
-    };
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self {
+            Self::Eof => "EOF",
+            Self::RArrow => "RightArrow",
+            Self::Semicolon => "Semicolon",
+            Self::Literal(_) => "Literal",
+            Self::Divide => "Divide",
+            Self::Ask => "Ask",
+            Self::NEqual => "NotEqual",
+            Self::Equal => "Equal",
+            Self::String => "String",
+            Self::Plus => "Plus",
+            Self::Asterisk => "Asterisk",
+            Self::LThan => "LessThan",
+            Self::GThan => "GreaterThan",
+            Self::Number(_) => "Number",
+            Self::RParen => "RightParenthese",
+            Self::LParen => "LeftParenthese",
+            Self::RSquare => "RightSquareBracket",
+            Self::LSquare => "LeftSquareBracket",
+            Self::RSquirly => "RightSquirlyBracket",
+            Self::LSquirly => "LeftSquirlyBracket",
+            Self::Subtract => "Subtract",
+            Self::Illegal => "Illegal",
+            Self::Inputs => "Inputs",
+            Self::GThanOrEqual => "GreatherThanOrEqual",
+            Self::LThanOrEqual => "LessThanOrEqual",
+            Self::Comma => "Comma",
+            Self::Ident(_) => "Identifier",
+            Self::Colon => "Colon",
+            Self::Multichoice => "Multichoice",
+            Self::Calculation => "Calculation",
+        };
+        write!(f, "{}", name)
+    }
 }
