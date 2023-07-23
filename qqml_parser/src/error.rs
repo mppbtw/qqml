0edea2c8a7cfd90143d442a4787d61d041977780
mppbtw <mppdevv@skiff.com>
2023-07-23 23:11:06 +0100

    added names for tokens

--- qqml_parser/src/error.rs
+++ qqml_parser/src/error.rs
@@ -1,9 +1,11 @@
 use qqml_lexer::Token;
+use std::fmt;
 
 #[derive(Debug, Clone)]
 pub enum Error {
     NonAsciiInput,
-    ExpectedToken(Token),
+    /// Stores the expected token/s, and then the token we recieved.
+    ExpectedToken(Vec<Token>, Token),
     UnexpectedToken(Token),
 }
 
@@ -12,5 +14,23 @@ impl From<qqml_lexer::Error> for Error {
         match value {
             qqml_lexer::Error::NonAsciiInput => Self::NonAsciiInput,
         }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let msg = match self {
+            Self::UnexpectedToken(t) => format!("Unexpected token: {}", t),
+            Self::ExpectedToken(t, g) => {
+                if t.len() == 1 {
+                    format!("Expected token {}, got {}", t[0], g)
+                } else {
+                    let token_names = t.iter().map(|t| format!("{}", t)) .collect::<Vec<String>>();
+                    format!("Expected one of {}, got {}", token_names.join(", "), g)
+                }
+            },
+            Self::NonAsciiInput => "QQML doesn't support Unicode just yet!".to_owned(),
+        };
+        write!(f, "{}", msg)
     }
 }
