90612a7ec7dd2a41246bfe54322145aafa9daea9
MrPiggyPegasus <charbalone@gmail.com>
2023-07-23 18:19:32 +0100

    worked some magic

---
+++ qqml_lexer/target/debug/deps/lazy_static-55299e210f3a54bc.d
@@ -0,0 +1,8 @@
+/home/mpp/dev/yarr/qqml/target/debug/deps/lazy_static-55299e210f3a54bc.rmeta: /home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/mpp/dev/yarr/qqml/target/debug/deps/liblazy_static-55299e210f3a54bc.rlib: /home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/mpp/dev/yarr/qqml/target/debug/deps/lazy_static-55299e210f3a54bc.d: /home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs:
+/home/mpp/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs:
---
+++ qqml_lexer/target/debug/deps/qqml-27010996aea22b2f.d
@@ -0,0 +1,10 @@
+/home/mpp/dev/yarr/qqml_lexer/target/debug/deps/qqml-27010996aea22b2f.rmeta: src/lib.rs src/error.rs src/lexer.rs src/token.rs
+
+/home/mpp/dev/yarr/qqml_lexer/target/debug/deps/libqqml-27010996aea22b2f.rlib: src/lib.rs src/error.rs src/lexer.rs src/token.rs
+
+/home/mpp/dev/yarr/qqml_lexer/target/debug/deps/qqml-27010996aea22b2f.d: src/lib.rs src/error.rs src/lexer.rs src/token.rs
+
+src/lib.rs:
+src/error.rs:
+src/lexer.rs:
+src/token.rs:
