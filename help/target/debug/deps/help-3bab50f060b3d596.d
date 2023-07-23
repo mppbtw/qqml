1039858f837667bc78576ac315193b7b0e5cb46d
mppbtw <mppdevv@skiff.com>
2023-07-23 18:00:04 +0100

    scooby dooby doo where are you

---
+++ help/target/debug/deps/help-3bab50f060b3d596.d
@@ -0,0 +1,11 @@
+/home/mpp/dev/yarr/help/target/debug/deps/help-3bab50f060b3d596.rmeta: src/lib.rs src/section.rs src/yarr.rs src/section_play.rs Cargo.toml
+
+/home/mpp/dev/yarr/help/target/debug/deps/help-3bab50f060b3d596.d: src/lib.rs src/section.rs src/yarr.rs src/section_play.rs Cargo.toml
+
+src/lib.rs:
+src/section.rs:
+src/yarr.rs:
+src/section_play.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
