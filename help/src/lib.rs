fc2abab0907980bdba7487932f103b424d3522d2
MrPiggyPegasus <charbalone@gmail.com>
2023-07-23 15:06:00 +0100

    Added some help info

--- help/src/lib.rs
+++ help/src/lib.rs
@@ -1,10 +1,32 @@
-pub fn print_help(command: HelpCommand) {}
+use std::process::exit;
+
+mod section;
+mod yarr;
+mod section_play;
+
+pub const TOPTEXT: &str = "\
+Yet Another Revision Resource - The pointless and overkill homework project.
+Yarr is free software, licensed under the GNU GPL v3.0 and the source code is
+available at https://github.com/MrPiggyPegasus/yarr
+
+";
+
+pub fn print_help(command: HelpCommand) {
+    let help_text = match command {
+        HelpCommand::Yarr => yarr::HELP.to_owned(),
+        HelpCommand::Section => section::HELP.to_owned(),
+        HelpCommand::SectionPlay => section_play::HELP.to_owned(),
+        _ => "abc".to_owned(),
+    };
+    println!("{}", help_text);
+    exit(0);
+}
 
 pub enum HelpCommand {
-    Play,
+    SectionPlay,
     Version,
     Yarr,
     Init,
-    Sections,
+    Section,
     Lint,
 }
