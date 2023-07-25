0e63cb915db8fc1b1ec6df8ce670ca83ccd6effe
mppbtw <mppdevv@skiff.com>
2023-07-25 11:01:51 +0100

    added some more argparsing to yarr qqml check

--- src/commands/qqml/check.rs
+++ src/commands/qqml/check.rs
@@ -1,5 +1,8 @@
 use std::process::exit;
+use std::fs;
+
 use crate::utils::*;
+
 use help::*;
 
 pub fn check() -> ! {
@@ -16,5 +19,36 @@ pub fn check() -> ! {
         exit(0);
     }
 
+    let mut verbose = false;
+    if arg == *"-v" || arg == *"--verbose" {
+        verbose = true;
+    }
+
+    if arg.starts_with('-') {
+        print_error(format!("Unrecognised option {}", arg));
+    }
+
+    let mut buf = String::new();
+    match fs::read_to_string(&mut buf) {
+        Ok(_) => (),
+        Err(e) => {
+            print_error(format!("Failed to read file '{}': {}", arg, e))
+        }
+    }
+
+    match next_arg() {
+        Some(a) => {
+            if a == *"-v" || a == *"--verbose" {
+                verbose = true;
+            }
+        }
+        None => ()
+    }
+
+    if verbose {
+        println!("*flamboyantly* looks good to me!");
+    } else {
+        println!("ok i think")
+    }
     exit(1);
 }
