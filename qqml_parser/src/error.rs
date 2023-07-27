7beb67c856778dd63eba14b7df3a8d18bea2c054
mppbtw <mppdevv@skiff.com>
2023-07-27 21:14:23 +0100

    added boilerplate code to parse hints directive

--- qqml_parser/src/error.rs
+++ qqml_parser/src/error.rs
@@ -2,8 +2,9 @@ use qqml_lexer::Token;
 
 #[derive(Debug, Clone)]
 pub enum Error {
-    NonAsciiInput,
-    /// Stores the expected token/s, and then the token we recieved plus any extra info for the end user.
-    ExpectedToken(Vec<Token>, Token, String),
-    UnexpectedToken(Token),
+    HintsDirectiveRequiresNumber,
+    HintsDirectiveRepeated,
+
+    /// Stores the token which we got instead
+    ExpectedQuestionOrDirective(Token),
 }
