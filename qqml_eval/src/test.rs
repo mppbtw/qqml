b2f930eaf0fd54be25b1ebd3d5788d2f0ca20631
mppbtw <mppdevv@skiff.com>
2023-07-27 14:54:44 +0100

    shouldntve commited that ...

--- qqml_eval/src/test.rs
+++ qqml_eval/src/test.rs
@@ -1,7 +1,7 @@
 use super::utils::*;
 
 #[test]
-fn do_the_roar() {
+fn test_get_term_size() {
     // The only good way to test this function unfortunately.
     dbg!(get_term_size().unwrap().width);
     dbg!(get_term_size().unwrap().height);
