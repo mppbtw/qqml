1098de840c15bdd66915855c94fd5d021f00b9f7
mppbtw <mppdevv@skiff.com>
2023-07-27 21:14:23 +0100

    added boilerplate code to parse hints directive

--- qqml_parser/src/lib.rs
+++ qqml_parser/src/lib.rs
@@ -1,9 +1,14 @@
 mod error;
 mod multichoice;
+mod parser;
+mod warning;
 
+pub use self::warning::Warning;
 pub use self::error::Error;
 pub use self::multichoice::MultichoiceAnswer;
 pub use self::multichoice::MultichoiceData;
+pub use self::parser::parse;
+pub use self::parser::ParsedFile;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Question {
---
+++ qqml_parser/src/parser.rs
@@ -0,0 +1,74 @@
+use qqml_lexer::Lexer;
+use crate::Error;
+use qqml_lexer::Token;
+use crate::Question;
+use crate::Warning;
+
+pub struct ParsedFile {
+    pub questions: Vec<Question>,
+    pub max_hints: usize,
+    pub warnings: Vec<Warning>
+}
+
+pub struct ErrorReport {
+    pub errors: Vec<Error>,
+    pub warnings: Vec<Warning>
+}
+impl ErrorReport {
+    pub fn new() -> ErrorReport {
+        ErrorReport {
+            errors: vec![],
+            warnings: vec![],
+        }
+    }
+}
+
+pub fn parse(inp: String) -> Result<ParsedFile, Vec<Error>> {
+    let mut max_hints = 0;
+    let mut hints_directive_seen = false;
+    let mut l = Lexer::new(inp);
+
+    let mut report = ErrorReport::new();
+
+    loop {
+        let mut tok = l.next_token();
+        if tok == Token::Semicolon {
+            break;
+        }
+
+        // Parse 'hints' directive
+        if tok == Token::Hints {
+            if hints_directive_seen {
+                report.errors.push(Error::HintsDirectiveRepeated);
+                continue;
+            } else {
+                hints_directive_seen = true;
+                tok = l.next_token();
+                match tok {
+                    Token::Number(n) => max_hints = n,
+                    _ => report.errors.push(Error::HintsDirectiveRepeated),
+                }
+                continue
+            }
+        }
+
+        // Parse questions
+        if tok == Token::Ask {
+            // Get the type of the next token
+            tok = l.next_token();
+            match tok {
+                Token::Multichoice => (),
+                _ => (),
+            }
+            continue;
+        }
+
+        report.errors.push(Error::ExpectedQuestionOrDirective(tok));
+    }
+
+    Ok(ParsedFile {
+        questions: vec![],
+        max_hints,
+        warnings: vec![],
+    })
+}
