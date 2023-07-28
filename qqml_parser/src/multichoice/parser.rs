1115e7629f62dc246e7beb6552559650ec585b68
MrPiggyPegasus <charbalone@gmail.com>
2023-07-28 20:42:48 +0100

    added parsing for multichoice

--- qqml_parser/src/multichoice/parser.rs
+++ qqml_parser/src/multichoice/parser.rs
@@ -1,7 +1,65 @@
-use qqml_lexer::Lexer;
-use crate::MultichoiceData;
 use crate::error::ErrorReport;
+use crate::Error;
+use crate::{MultichoiceAnswer, MultichoiceData};
+use qqml_lexer::{Lexer, Token};
 
 pub fn parse_multichoice(l: &mut Lexer) -> Result<MultichoiceData, ErrorReport> {
+    // This function expects the 'ask' and 'multichoice' tokens
+    // to have already been consumed by it's caller.
+
+    let mut report = ErrorReport::new();
+    let mut tok = l.next_token();
+    let mut dat = MultichoiceData::default();
+
+    if tok != Token::LParen {
+        report
+            .errors
+            .push(Error::ExpectedLParenForQuestionMaxMark(tok));
+    }
+
+    tok = l.next_token();
+    match tok {
+        Token::Number(n) => dat.max_marks = Some(n),
+        _ => report
+            .errors
+            .push(Error::ExpectedNumberForQuestionMaxMark(tok)),
+    };
+
+    tok = l.next_token();
+    if tok != Token::RParen {
+        report
+            .errors
+            .push(Error::ExpectedRParenForQuestionMaxMark(tok));
+    }
+
+    tok = l.next_token();
+    match tok {
+        Token::Literal(l) => dat.text = Some(l),
+        _ => report.errors.push(Error::ExpectedQuestionText(tok)),
+    };
+
+    tok = l.next_token();
+    if tok != Token::LSquirly {
+        report.errors.push(Error::ExpectedLSquirlyForQuestion(tok));
+    }
+
+    loop {
+        tok = l.next_token();
+        if tok == Token::RParen {
+            break;
+        }
+        if tok == Token::Asterisk {
+            match parse_multichoice_question(l) {
+                Ok(a) => dat.answers.push(a),
+                Err(r) => report.extend(r),
+            }
+        }
+    }
+
     Ok(MultichoiceData::default())
+}
+
+pub fn parse_multichoice_question(l: &mut Lexer) -> Result<MultichoiceAnswer, ErrorReport> {
+    let mut dat = MultichoiceAnswer::default();
+    Ok(dat)
 }
--- qqml_parser/src/parser.rs
+++ qqml_parser/src/parser.rs
@@ -1,16 +1,16 @@
-use qqml_lexer::Lexer;
 use crate::error::ErrorReport;
-use crate::Error;
 use crate::multichoice::parse_multichoice;
-use qqml_lexer::Token;
+use crate::Error;
 use crate::Question;
 use crate::Warning;
+use qqml_lexer::Lexer;
+use qqml_lexer::Token;
 
 #[derive(Clone, Default, Debug, PartialEq, Eq)]
 pub struct ParsedFile {
     pub questions: Vec<Question>,
     pub max_hints: usize,
-    pub warnings: Vec<Warning>
+    pub warnings: Vec<Warning>,
 }
 
 pub fn parse(inp: String) -> Result<ParsedFile, Vec<Error>> {
@@ -39,24 +39,27 @@ pub fn parse(inp: String) -> Result<ParsedFile, Vec<Error>> {
                     Token::Number(n) => max_hints = n,
                     _ => report.errors.push(Error::HintsDirectiveRepeated),
                 }
-                continue
+                tok = l.next_token();
+                if tok != Token::Semicolon {
+                    report
+                        .errors
+                        .push(Error::ExpectedSemicolonAfterHintsDirective(tok));
+                }
+                continue;
             }
         }
 
         // Parse questions
         if tok == Token::Ask {
-
             // Get the type of the next token
             tok = l.next_token();
             match tok {
-                Token::Multichoice => {
-                    match parse_multichoice(&mut l) {
-                        Ok(data) => output.questions.push(Question::Multichoice(data)),
-                        Err(r) => report.extend(r),
-                    }
+                Token::Multichoice => match parse_multichoice(&mut l) {
+                    Ok(data) => output.questions.push(Question::Multichoice(data)),
+                    Err(r) => report.extend(r),
                 },
                 Token::Ident(_) => report.errors.push(Error::InvalidQuestionType(tok)),
-                _ => ()
+                _ => report.errors.push(Error::ExpectedQuestionType(tok)),
             }
             continue;
         }
