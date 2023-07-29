e0a5166f8ffab775535db00a3935e965aec4332d
mppbtw <mppdevv@skiff.com>
2023-07-29 20:14:38 +0100

    design patterns elements of reusable object-oriennted software

---
+++ qqml_parser/src/multichoice/tests/question.rs
@@ -0,0 +1,10 @@
+#[test]
+fn test_parse_multichoice() {
+    let input = "
+        (1) 'title' {
+            * 'correct' (1) -> 'explanation';
+            * 'incorrect' -> 'explanation';
+            * 'inc';
+        };
+        ";
+}
