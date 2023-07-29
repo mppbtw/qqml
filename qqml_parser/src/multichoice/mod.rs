53304e441ef5df054fe78ff6729a5ae7f9d0afad
mppbtw <mppdevv@skiff.com>
2023-07-29 18:32:43 +0100

    added some tests

--- qqml_parser/src/multichoice/mod.rs
+++ qqml_parser/src/multichoice/mod.rs
@@ -1,5 +1,9 @@
 mod data;
 mod parser;
 
+#[cfg(test)]
+mod tests;
+
 pub use self::data::*;
 pub use self::parser::parse_multichoice;
+pub use self::parser::parse_multichoice_answer;
--- qqml_parser/src/test/mod.rs
+++ qqml_parser/src/test/mod.rs
@@ -2,7 +2,13 @@ use crate::*;
 
 #[test]
 fn test_parse_hints_directive() {
-    let input = "
+    let input1 = "
+        hints 1;
+        ".to_string();
+    assert_eq!(parse(input1).unwrap().max_hints, 1);
+
+    let input2 = "
         hints 2;
-        ";
+        ".to_string();
+    assert_eq!(parse(input2).unwrap().max_hints, 2);
 }
