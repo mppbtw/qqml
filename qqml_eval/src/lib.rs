e1c697a02257e975650f9e4817b8c1e683aefc67
MrPiggyPegasus <charbalone@gmail.com>
2023-08-05 10:52:31 +0100

    finalised some designs

--- qqml_eval/src/lib.rs
+++ qqml_eval/src/lib.rs
@@ -11,16 +11,42 @@ pub use error::Error;
 
 // Example TUI:
 
-// ~/.yarr/sections/geography.qqml
+//          ~/.qpm/local/gcse/geography/countries.qqml // These lines should be centred
+//            QQML Version 1.0.0, press ? for help     // by the user's terminal width
 //
 // <-(23/25)-> // h and l to move between questions
 //
 // Where is France? (2)
 //
-// Europe   <    // j and k to select the answer
-// Asia
+// Europe   <    // j and k to select the answer and space
+// Asia          // to confirm (will become highlighted)
 // Africa
 //
-// Hints: // Will appear as 'Hint:' when only one hint is present
+// Hints (3 remaining): // This is the number of hints the question has left
 //  France is west of Germany which is to the north of Italy
+//
+//  France is not in Africa
+
+
+
+// After answering the question:
+
+//          ~/.qpm/local/gcse/geography/countries.qqml
+//            QQML Version 1.0.0, press ? for help
+//
+// <-(23/25)-> // h and l to move between questions
+// hints: 5 // How many hints the player has left in total
+//
+// Where is France? (2)
+//
+// Europe   France is located in western Europe        // Word wrap
+//          bordering other countries such as Germany
+//          and Spain.
+//
+// Asia     France is not located in Asia.
+// Africa
+//
+// Hints (0 remaining): // Hints are no longer available
+//  France is west of Germany which is to the north of Italy
+//
 //  France is not in Africa
