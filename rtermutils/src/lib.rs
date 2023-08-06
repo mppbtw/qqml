67a9f3d05af9537e85547b3ff0e1d8f6bc6efc1e
mppbtw <mppdevv@skiff.com>
2023-08-06 21:26:15 +0100

    spent all day tryna do this better but no :(

--- rtermutils/src/lib.rs
+++ rtermutils/src/lib.rs
@@ -1,8 +1,7 @@
 #[link(name = "termutils", kind = "static")]
 extern "C" {
-     pub fn clear_screen_with_width() -> i32;
-     pub fn clear_screen_with_height() -> i32;
-     pub fn enter_alt_screen();
-     pub fn exit_alt_screen();
-     pub fn read_single_char() -> u8;
+    pub fn get_cursor_lines() -> i32;
+    pub fn get_cursor_cols() -> i32;
+    pub fn clear_screen_with_height() -> i32;
+    pub fn clear_screen_with_width() -> i32;
 }
