65e3e7ad8e48b7f18433f11a5f49b24c97597580
mppbtw <mppdevv@skiff.com>
2023-08-06 12:22:29 +0100

    added more stuff in c

--- rlibnocrates/src/lib.rs
+++ rlibnocrates/src/lib.rs
@@ -1,4 +1,7 @@
 #[link(name = "nocrates", kind = "static")]
 extern "C" {
      pub fn read_single_char() -> u8;
+     pub fn switch_to_alt_screen();
+     pub fn return_from_alt_screen();
+     pub fn clear_screen_with_width() -> i32;
 }
