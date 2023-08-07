e27ac82257ca71381475084d365971856418fcb1
mppbtw <mppdevv@skiff.com>
2023-08-07 18:25:26 +0100

    one can now change the question

--- qqml_eval/src/state.rs
+++ qqml_eval/src/state.rs
@@ -1,19 +1,16 @@
-use qqml_parser::Question;
-
 use crate::render::*;
+use qqml_parser::Question;
+use rtermutils::*;
 
 #[derive(Debug, Clone)]
 pub struct StateConstructor {
-    pub hints_used: usize,
     pub max_hints: usize,
     pub questions: Vec<Question>,
-    pub current_question_index: usize,
     pub path_to_source: Option<String>,
 }
 impl StateConstructor {
     pub fn construct(self) -> State {
         State {
-            hints_used: self.hints_used,
             max_hints: self.max_hints,
             questions: self.questions,
             path_to_source: self.path_to_source,
@@ -31,13 +28,23 @@ pub struct State {
     pub path_to_source: Option<String>,
     questions_len: usize,
     current_hints_available: usize,
+    cols: usize,
 }
 impl State {
-    pub fn create_screen(&self) -> Screen {
+    pub fn create_screen(&mut self) -> Screen {
+        self.cols = unsafe { clear_screen_with_width() } as usize;
+        self.questions_len = self.questions.len();
+        self.current_hints_available = match &self.questions[self.current_question_index] {
+            Question::Multichoice(d) => d.hints.len(),
+            _ => 0,
+        };
         Screen {
             pathline: {
                 match &self.path_to_source {
-                    Some(p) => Some(PathLine { path: &p }),
+                    Some(p) => Some(PathLine {
+                        path: &p,
+                        cols: &self.cols,
+                    }),
                     None => None,
                 }
             },
@@ -51,36 +58,40 @@ impl State {
                 },
                 hints_used_total: &self.hints_used,
             },
-            version_line: VersionLine,
+            version_line: VersionLine { cols: &self.cols },
             q_select_line: QuestionSelectLine {
                 max_questions: &(self.questions_len),
                 current_question: &self.current_question_index,
+                cols: &self.cols,
             },
-            question_line: QuestionLine { q: &self.questions[self.current_question_index] },
+            question_line: QuestionLine {
+                q: &self.questions[self.current_question_index],
+            },
             question_body: QuestionBody {
                 answers: {
                     match &self.questions[self.current_question_index] {
-                        Question::Multichoice(d) => d.answers.iter().map(|a| a.text.clone().unwrap()).collect(),
+                        Question::Multichoice(d) => {
+                            d.answers.iter().map(|a| a.text.clone().unwrap()).collect()
+                        }
                         _ => vec![],
-                    }.clone() // We can't move any data from this struct
+                    }
+                    .clone() // We can't move any data from this struct
                 },
                 selected: {
                     match &self.questions[self.current_question_index] {
                         Question::Multichoice(d) => &d.chosen_answer,
                         _ => &0,
                     }
-                }
+                },
             },
             hints_body: {
                 match &self.questions[self.current_question_index] {
-                    Question::Multichoice(d) => {
-                        Some(HintsBody {
-                            hints: &d.hints[0..d.used_hints],
-                        })
-                    }
-                    _ => None
+                    Question::Multichoice(d) => Some(HintsBody {
+                        hints: &d.hints[0..d.used_hints],
+                    }),
+                    _ => None,
                 }
-            }
+            },
         }
     }
 }
--- rtermutils/src/lib.rs
+++ rtermutils/src/lib.rs
@@ -4,4 +4,7 @@ extern "C" {
     pub fn get_cursor_cols() -> i32;
     pub fn clear_screen_with_height() -> i32;
     pub fn clear_screen_with_width() -> i32;
+    pub fn read_single_char() -> u8;
+    pub fn enter_alt_screen();
+    pub fn exit_alt_screen();
 }
