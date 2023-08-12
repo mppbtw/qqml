b367f65d748a1138f7865fb3f1351825206ac2d7
mppbtw <mppdevv@skiff.com>
2023-08-12 17:17:50 +0100

    wrote error messages

--- qqml_parser/src/error.rs
+++ qqml_parser/src/error.rs
@@ -6,8 +6,8 @@ use qqml_lexer::TokenData;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Error {
-    HintsDirectiveRequiresNumber,
-    HintsDirectiveRepeated,
+    HintsDirectiveRequiresNumber(Token),
+    HintsDirectiveRepeated(Token),
 
     /// Stores the data of where the literal began.
     UnterminatedLiteral(TokenData),
@@ -81,9 +81,95 @@ impl Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let msg = "ERROR MESSAGE PLACEHOLDER".to_owned();
-
-        let _ = msg.replace(
+        let msg = match self {
+            Self::ExpectedQuestionText(t) => {
+                format!("Expected text for the question, found {}.", t)
+            }
+            Self::MaxMarkIsZero(_) => {
+                "If you ever feel stupid, just remember that one complete and utter ".to_owned()
+                    + "baffoon decided to make a question worth 0 marks. Sacriligious."
+            }
+            Self::IntegerTooLarge(_) => format!(
+                "The parsing process could not continue because the maximum \"
+                representable number in QQML on your machine is {}.",
+                usize::MAX
+            ),
+            Self::ExpectedHintText(t) => {
+                if matches!(t, Token::Comma(_)) {
+                    "Please, to help with the current financial crisis, dont use extra commas."
+                        .into()
+                } else {
+                    format!("Expected text for this hint, found {}", t)
+                }
+            }
+            Self::HintsDirectiveRepeated(_) => {
+                "Only one hints directive is allowed per file.".into()
+            }
+            Self::ImpossibleMaxMark(_) => {
+                "The maximum mark for this question is not achievable.".into()
+            }
+            Self::UnterminatedLiteral(_) => {
+                "What goes up must come down. This string is never terminated".into()
+            }
+            Self::ExpectedAnswerText(t) => format!("Expected text for this answer, found {}.", t),
+            Self::NoMultichoiceAnswers(_) | Self::OnlyOneMultichoiceAnswer(_) => {
+                "So the point of a multiple choice question is there ".to_owned()
+                    + "are multiple choices to pick, right?"
+            }
+            Self::UnexpectedBodyToken(t) => format!("Who put that there? {}. See doc#body.", t),
+            Self::HintsDirectiveRequiresNumber(t) => {
+                format!("Expected a max hints number, found {}.", t)
+            }
+            Self::ExpectedAnswerSemicolon(_) => "The answer should end in a semicolon.".into(),
+            Self::ExpectedCommaInHintsList(_) => {
+                "The list of hints should be comma separated. do u even english bru?".into()
+            }
+            Self::InvalidQuestionType(_) => {
+                format!("No such question type keyword, see doc#questions")
+            }
+            Self::ExpectedLParenForQuestionMaxMark(t) => format!(
+                "Expected a '(' to set the max mark of the question, found {}. See doc#questions",
+                t
+                ),
+            Self::ExpectedRParenForQuestionMaxMark(t) => format!(
+                "Expected a ')' to finish setting the max mark of the question, found {}. See doc#questions",
+                t
+                ),
+            Self::ExpectedNumberForAnswerMark(t) => format!(
+                "Expected a number for the answer's mark, found {}. See doc#body",
+                t
+                ),
+            Self::ExpectedNumberForQuestionMaxMark(t) => format!(
+                "Expected a number for the question's max mark, found {}. See doc#questions",
+                t
+                ),
+            Self::ExpectedRParenForAnswerMark(t) => format!(
+                "Expected a ')' to finish setting the mark of this answer, found {}. See doc#body",
+                t
+                ),
+            Self::ExpectedLSquirlyForQuestion(t) => format!(
+                "Expected a '{{' to start the body of the question, found {}. See doc#body",
+                t
+                ),
+            Self::ExpectedSemicolonAfterHintsDirective(t) =>
+                format!("Expected a semicolon after the hints directive, found {}. See doc#hints", t),
+            Self::ExpectedQuestionOrDirective(t) => format!(
+                "Expected either a question (ask) or a directive e.g hints, found {}. See doc#questions and doc#hints",
+                t
+                ),
+            Self::ExpectedQuestionType(t) => format!(
+                "Expected a question type keyword e.g. 'multichoice', found {}. See doc#questions",
+                t
+                ),
+            Self::UnexpectedAnswerToken(t, _) => format!(
+                "Unexpected answer token: {}. See doc#body",
+                t
+                ),
+            Self::ExpectedAnswerExplanationText(t) => format!(
+                "Expected the text for your answer explanation, found {}. See doc#body",
+                t
+                ),
+        }.replace(
             "doc",
             "https://github.com/MrPiggyPegasus/yarr/blob/main/doc/QQML.md",
         );
