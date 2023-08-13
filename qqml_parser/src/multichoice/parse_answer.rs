dea225e78511176e356ae8967611462e7c7f9813
mppbtw <mppdevv@skiff.com>
2023-08-13 19:54:52 +0100

    added negative parsing for answers

--- qqml_parser/src/multichoice/parse_answer.rs
+++ qqml_parser/src/multichoice/parse_answer.rs
@@ -67,11 +67,23 @@ pub fn parse_multichoice_answer(l: &mut Lexer) -> Result<MultichoiceAnswer, Erro
         if report.errors.len() == 1 {
             Err(report)
         } else {
-            let pos = positive_tolerance(&mut starting_lexer).unwrap_err();
-            if pos.errors.len() == 1 || pos.errors.len() < report.errors.len() {
-                Err(pos)
+            let pos = positive_tolerance(&mut starting_lexer.clone()).unwrap_err();
+            if pos.errors.len() == 1 {
+                return Err(pos);
+            }
+            let neg = negative_tolerance(&mut starting_lexer).unwrap_err();
+            if pos.errors.len() < neg.errors.len() {
+                if pos.errors.len() < report.errors.len() {
+                    Err(pos)
+                } else {
+                    Err(report)
+                }
             } else {
-                Err(report)
+                if neg.errors.len() < report.errors.len() {
+                    Err(neg)
+                } else {
+                    Err(report)
+                }
             }
         }
     } else {
@@ -79,6 +91,68 @@ pub fn parse_multichoice_answer(l: &mut Lexer) -> Result<MultichoiceAnswer, Erro
     }
 }
 
+pub fn negative_tolerance(l: &mut Lexer) -> Result<(), ErrorReport> {
+    let mut report = ErrorReport::default();
+    let mut tok = l.next_token()?;
+    let mut skip_next_tok = false;
+
+    match tok {
+        Token::Literal(..) => (),
+        _ => {
+            report.errors.push(Error::ExpectedAnswerText(tok.clone()));
+            skip_next_tok = true;
+        }
+    };
+
+    loop {
+        if skip_next_tok {
+            skip_next_tok = false;
+        } else {
+            tok = l.next_token()?;
+        }
+        if matches!(tok, Token::LParen(_)) {
+            tok = l.next_token()?;
+            match tok {
+                Token::Number(..) => tok = l.next_token()?,
+                _ => report.errors.push(Error::UnexpectedAnswerToken(
+                    tok.clone(),
+                    vec![
+                        Token::RArrow(TokenData::default()),
+                        Token::Semicolon(TokenData::default()),
+                        Token::Number(TokenData::default(), 0),
+                    ],
+                )),
+            };
+
+            if !matches!(tok, Token::RParen(_)) {
+                report.errors.push(Error::ExpectedRParenForAnswerMark(tok.clone()));
+            } else {
+                tok = l.next_token()?;
+            }
+        }
+
+        if matches!(tok, Token::RArrow(_)) {
+            tok = l.next_token()?;
+            match tok {
+                Token::Literal(..) => tok = l.next_token()?,
+                _ => report
+                    .errors
+                    .push(Error::ExpectedAnswerExplanationText(tok.clone())),
+            };
+        }
+
+        if !matches!(tok, Token::Semicolon(_)){
+            report.errors.push(Error::ExpectedAnswerSemicolon(tok.clone()));
+        } else {
+            break;
+        }
+        if matches!(tok, Token::Eof(_)) {
+            break;
+        }
+    }
+
+    Err(report)
+}
 fn positive_tolerance(l: &mut Lexer) -> Result<(), ErrorReport> {
     let mut report = ErrorReport::default();
     let mut tok;
