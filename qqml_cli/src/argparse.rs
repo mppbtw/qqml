8cf22fc2d73dee28b819eebe5d1db33abde9864b
MrPiggyPegasus <charbalone@gmail.com>
2023-08-13 13:24:56 +0100

    esfoij

--- qqml_cli/src/argparse.rs
+++ qqml_cli/src/argparse.rs
@@ -1,20 +1,21 @@
 use std::env::args;
 
-const OPTIONS: [&'static str; 6] = [
-    "-h", "--help",
-    "-v", "--version",
-    "-c", "--check",
-];
+const OPTIONS: [&str; 6] = ["-h", "--help", "-v", "--version", "-c", "--check"];
 
 pub fn has_help() -> bool {
     let args = args().collect::<Vec<String>>();
     args.contains(&"-h".to_owned()) || args.contains(&"--help".to_owned())
 }
 
+pub fn has_check() -> bool {
+    let args = args().collect::<Vec<String>>();
+    args.contains(&"-c".to_owned()) || args.contains(&"--check".to_owned())
+}
+
 pub fn get_file_arg() -> Option<String> {
     for a in &args().collect::<Vec<String>>()[1..] {
         if !OPTIONS.contains(&a.as_str()) {
-            return Some(a.clone())
+            return Some(a.clone());
         }
     }
     None
