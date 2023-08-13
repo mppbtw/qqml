f92ec2a74b502ac6c07cb8d9da032306c72ebefc
mppbtw <mppdevv@skiff.com>
2023-08-13 17:59:03 +0100

    refactored and wrote tests to convert parse_multichoice_answer to tripartite parsing

--- qqml_parser/src/multichoice/parser.rs
+++ qqml_parser/src/multichoice/parser.rs
@@ -1,19 +1,21 @@
 use crate::error::ErrorReport;
+use super::parse_answer::parse_multichoice_answer;
 use crate::Error;
-use crate::MultichoiceAnswer;
 use crate::MultichoiceData;
 use qqml_lexer::Lexer;
 use qqml_lexer::Token;
-use qqml_lexer::TokenData;
 
 /// 't' is the token for the ask keyword used to attach some more metadata to the data
 pub fn parse_multichoice<T: Into<Token>>(
     l: &mut Lexer,
     keyword: T,
 ) -> Result<MultichoiceData, ErrorReport> {
+    // This parser is replacement tolerant and
+    // expects the 'ask' and 'multichoice' tokens
+    // to have already been consumed by the top
+    // level parser.
+
     let keyword = keyword.into();
-    // This function expects the 'ask' and 'multichoice' tokens
-    // to have already been consumed by it's caller.
 
     let mut report = ErrorReport::new();
     let mut tok = l.next_token()?;
@@ -150,64 +152,6 @@ pub fn parse_multichoice<T: Into<Token>>(
     }
 
     dat.line = keyword.get_data().line;
-
-    if !report.errors.is_empty() {
-        Err(report)
-    } else {
-        Ok(dat)
-    }
-}
-
-pub fn parse_multichoice_answer(l: &mut Lexer) -> Result<MultichoiceAnswer, ErrorReport> {
-    // This function assumes that the '*' token has already been
-    // consumed and therefore assumes that the next token should
-    // be the question text.
-    let mut report = ErrorReport::default();
-    let mut dat = MultichoiceAnswer::default();
-    let mut tok = l.next_token()?;
-
-    match tok {
-        Token::Literal(_, m) => dat.text = Some(m),
-        _ => report.errors.push(Error::ExpectedAnswerText(tok)),
-    };
-
-    tok = l.next_token()?;
-    if matches!(tok, Token::LParen(_)) {
-        tok = l.next_token()?;
-        match tok {
-            Token::Number(_, n) => dat.marks = n,
-            _ => report.errors.push(Error::UnexpectedAnswerToken(
-                tok,
-                vec![
-                    Token::RArrow(TokenData::default()),
-                    Token::Semicolon(TokenData::default()),
-                    Token::Number(TokenData::default(), 0),
-                ],
-            )),
-        };
-
-        tok = l.next_token()?;
-        if !matches!(tok, Token::RParen(_)) {
-            report.errors.push(Error::ExpectedRParenForAnswerMark(tok));
-        }
-
-        tok = l.next_token()?;
-    }
-
-    if matches!(tok, Token::RArrow(_)) {
-        tok = l.next_token()?;
-        match tok {
-            Token::Literal(_, l) => dat.explanation = Some(l),
-            _ => report
-                .errors
-                .push(Error::ExpectedAnswerExplanationText(tok)),
-        };
-        tok = l.next_token()?;
-    }
-
-    if !matches!(tok, Token::Semicolon(_)) {
-        report.errors.push(Error::ExpectedAnswerSemicolon(tok));
-    }
 
     if !report.errors.is_empty() {
         Err(report)
