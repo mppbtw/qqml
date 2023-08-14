63713faeb831e8c540d32b8dd58cd2f64fd19f21
MrPiggyPegasus <charbalone@gmail.com>
2023-08-14 22:27:52 +0100

    made the report picking faster

--- qqml_parser/src/multichoice/parser.rs
+++ qqml_parser/src/multichoice/parser.rs
@@ -17,7 +17,6 @@ pub fn parse_multichoice<T: Into<Token>>(
 
     let keyword = keyword.into();
 
-    let starting_l = l.clone();
     let starting_l_data = l.get_lexer_data();
     let mut report = ErrorReport::new();
     let mut tok = l.next_token()?;
@@ -147,27 +146,38 @@ pub fn parse_multichoice<T: Into<Token>>(
     dat.line = keyword.get_data().line;
 
     if !report.errors.is_empty() {
-        let pos = positive_tolerance(&mut Lexer::from_lexer_data(
-            l.get_input(),
-            starting_l_data.clone(),
-        ))
-        .unwrap_err();
-        let neg = negative_tolerance(&mut Lexer::from_lexer_data(
-            l.get_input(),
-            starting_l_data.clone(),
-        ))
-        .unwrap_err();
-        if neg.errors.len() < pos.errors.len() {
-            if neg.errors.len() < report.errors.len() {
-                Err(neg)
-            } else {
-                Err(report)
-            }
+        if report.errors.len() == 1 {
+            Err(report)
         } else {
-            if pos.errors.len() < report.errors.len() {
+            let pos = positive_tolerance(&mut Lexer::from_lexer_data(
+                l.get_input(),
+                starting_l_data.clone(),
+            ))
+            .unwrap_err();
+            if pos.errors.len() == 1 {
                 Err(pos)
+            } else if pos.errors.len() < report.errors.len() {
+                let neg = negative_tolerance(&mut Lexer::from_lexer_data(
+                    l.get_input(),
+                    starting_l_data.clone(),
+                ))
+                .unwrap_err();
+                if neg.errors.len() < pos.errors.len() {
+                    Err(neg)
+                } else {
+                    Err(pos)
+                }
             } else {
-                Err(report)
+                let neg = negative_tolerance(&mut Lexer::from_lexer_data(
+                    l.get_input(),
+                    starting_l_data.clone(),
+                ))
+                .unwrap_err();
+                if neg.errors.len() < report.errors.len() {
+                    Err(neg)
+                } else {
+                    Err(report)
+                }
             }
         }
     } else {
