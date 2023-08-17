2e1ab801c80c1c0595217b560bf1ab297c3db145
MrPiggyPegasus <charbalone@gmail.com>
2023-08-17 14:37:14 +0100

    added some cli stuff

--- qpm/cmd/main.go
+++ qpm/cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"github.com/mrpiggypegasus/qqml/internal/argparsing"
 )
 
 func main() {
-    fmt.Println("hello world");
+	switch internal.NextArg() {
+	case "":
+		fmt.Println("No arguments supplied. Run qpm --help for more info")
+	case "version":
+		versionMessage()
+	default:
+		helpScreen()
+	}
+}
+
+func helpScreen() {
+	fmt.Println("QPM v1.0 (c) 2023 'MrPiggyPegasus'")
+}
+
+func versionMessage() {
+	fmt.Println("QPM v1.0 (c) 2023 'MrPiggyPegasus'",)
 }
