9da3936e6eb7785041c15227cba22ce87a3fc423
mppbtw <mppdevv@skiff.com>
2023-08-17 15:49:45 +0100

    added initialisilisitiilissation for qpm

--- qpm/internal/argparsing/argparsing.go
+++ qpm/internal/argparsing/argparsing.go
@@ -17,3 +17,12 @@ func NextArg() string {
     return getArgs()[current_arg]
 }
 
+func HasVerbose() bool {
+    args := getArgs();
+    for i:=0; i<len(args); i++ {
+        if args[i] == "-V" || args[i] == "--verbose" {
+            return true
+        }
+    }
+    return false
+}
