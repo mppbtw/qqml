4a9990e00f98bb1a98010dd9bf260daf92440457
mppbtw <mppdevv@skiff.com>
2023-08-17 10:14:14 +0100

    prevented construction of the json return value if it wont be used anyway

--- qqml_eval/src/end_screen.rs
+++ qqml_eval/src/end_screen.rs
@@ -34,13 +34,17 @@ pub fn end_screen(s: &mut State, log_path: Option<String>) {
     println!("You got {}/{}", s.achieved_marks(), s.get_max_marks());
     println!("Press any to continue, return to view your answers again.");
     if unsafe { read_single_char() } != b'\n' {
-        cleanup_and_exit(
-            Some(format!(
-                "{{\"finished\": true, \"file_data\":{} }}",
-                s.to_json()
-            )),
-            log_path,
-        );
+        if log_path.is_some() {
+            cleanup_and_exit(
+                Some(format!(
+                    "{{\"finished\": true, \"file_data\":{} }}",
+                    s.to_json()
+                )),
+                log_path,
+            );
+        } else {
+            cleanup_and_exit(None, log_path);
+        }
     }
 }
 
