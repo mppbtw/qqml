5ee654012dee03ccba28400eda515b2453a1e5eb
mppbtw <mppdevv@skiff.com>
2023-08-18 03:44:35 +0100

    some more qpm cli stuff

--- qpm/cmd/main.go
+++ qpm/cmd/main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/mrpiggypegasus/qqml/internal/argparsing"
+	"github.com/mrpiggypegasus/qqml/internal/cmds"
 	"os"
 )
 
 func main() {
-	switch internal.NextArg() {
+	switch argparsing.NextArg() {
 	case "":
 		fmt.Println("No arguments supplied. Run qpm --help for more info")
 	case "version":
 		versionMessage()
 	case "init":
 		initConfig()
+	case "run":
+		{
+			arg := argparsing.NextArg()
+			if len(arg) == 0 {
+				fmt.Println("Expected another argument <FILE>.")
+				os.Exit(1)
+			}
+			cmds.Run(arg)
+		}
 	default:
 		helpScreen()
 	}
@@ -28,9 +38,9 @@ func versionMessage() {
 }
 
 func initConfig() {
-	verbose := true;
+	verbose := true
 
-	homeDir, err := os.UserHomeDir();
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Failed to get user home directory. Are you root?")
 		return
@@ -42,8 +52,8 @@ func initConfig() {
 			fmt.Println("~/.qpm/ already exists.")
 		}
 	} else {
-		if os.Mkdir(homeDir + ".qpm/", 0755) != nil {
-			fmt.Fprintf(os.Stderr, "Failed to create directory ~/.qpm\n")
+		if os.Mkdir(homeDir+".qpm/", 0755) != nil {
+			fmt.Println("Failed to create directory ~/.qpm")
 			return
 		}
 	}
@@ -55,7 +65,7 @@ func initConfig() {
 	} else {
 		_, err := os.Create(homeDir + ".qpm/repos.cfg")
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to create file ~/.qpm/repos.cfg\n")
+			fmt.Println("Failed to create file ~/.qpm/repos.cfg")
 			return
 		}
 	}
@@ -65,14 +75,14 @@ func initConfig() {
 			fmt.Println("~/.qpm/local/ already exists.")
 		}
 	} else {
-		if os.Mkdir(homeDir + ".qpm/local/", 0755) != nil {
-			fmt.Fprintf(os.Stderr, "Failed to create directory ~/.qpm/local/\n")
+		if os.Mkdir(homeDir+".qpm/local/", 0755) != nil {
+			fmt.Println("Failed to create directory ~/.qpm/local/")
 			return
 		}
 	}
 }
 
 func doesPathExist(path string) bool {
-	_, err := os.Stat(path);
+	_, err := os.Stat(path)
 	return err == nil
 }
