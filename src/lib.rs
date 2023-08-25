32b727db3f51c78fb5ee18bbe14b28b6bde7ae71
mppbtw <mppdevv@skiff.com>
2023-08-25 17:10:25 +0100

    most of the deserialising to state is done

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
+mod eval;
+mod json;
 mod lexer;
 mod parser;
 
+pub use eval::diagnostics::render_error;
+pub use eval::repl::run;
 pub use lexer::core::Lexer;
 pub use lexer::core::LexerError;
 pub use lexer::token::Token;
 pub use lexer::token::TokenData;
 pub use parser::core::parse;
 pub use parser::core::ParsedFile;
+pub use parser::error::Error;
+pub use parser::error::ErrorReport;
 pub use parser::multichoice::parse_answer::parse_multichoice_answer;
 pub use parser::multichoice::parser::parse_multichoice;
 pub use parser::Question;
