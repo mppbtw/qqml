c49431955e0d3aea8bb35da04a9930d0a593ae3d
mppbtw <mppdevv@skiff.com>
2023-08-26 22:20:23 +0100

    added break_cursor_with_termsize for more goody goodness

--- rtermutils/src/lib.rs
+++ rtermutils/src/lib.rs
@@ -4,6 +4,16 @@ extern "C" {
     pub fn get_cursor_cols() -> i32;
     pub fn clear_screen_with_height() -> i32;
     pub fn clear_screen_with_width() -> i32;
+
+    // By 'break cursor' I mean that the cursor will be in some arbitrary place
+    // after calling the function. This is OK because the we might need to do
+    // more computation before clearing the screen to prevent flickering.
+    pub fn break_cursor_with_height() -> i32;
+
+    // By 'break cursor' I mean that the cursor will be in some arbitrary place
+    // after calling the function. This is OK because the we might need to do
+    // more computation before clearing the screen to prevent flickering.
+    pub fn break_cursor_with_width() -> i32;
     pub fn read_single_char() -> u8;
     pub fn enter_alt_screen();
     pub fn exit_alt_screen();
