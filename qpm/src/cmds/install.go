ed8299b59b92e620269f5a86c23865078b0e044a
mppbtw <mppdevv@skiff.com>
2023-09-01 06:39:09 +0100

    added installing to qpm

---
+++ qpm/src/cmds/install.go
@@ -0,0 +1,41 @@
+package cmds
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Install() {
+	var quiz string
+	flag.StringVar(&quiz, "q", "", "The quiz to Install")
+	flag.CommandLine.Parse(os.Args[2:])
+	if !IsFlagPassed("q") || quiz == "" {
+		fmt.Println("The -q flag is required")
+		os.Exit(1)
+	}
+    if FileExists(quiz) {
+        quizFileName := strings.Split(quiz, "/")[len(strings.Split(quiz, "/"))-1]
+        if len(strings.Split(quizFileName, ".")) == 3 {
+            fmt.Println("quizFileName names must only contain one dot")
+            os.Exit(1)
+        }
+        if len(strings.Split(quizFileName, ".")) == 1 {
+            fmt.Println("quizFileName names must have the .qqml file extension<F4>")
+            os.Exit(1)
+        }
+        if strings.Split(quizFileName, ".")[1] != "qqml" {
+            fmt.Println("quizFileName names must have the .qqml file extension<F4>")
+            os.Exit(1)
+        }
+
+        // Everything has been validated
+        destPath := GetHomeDir() + ".qpm/local/src/" + quizFileName
+        _, err := CopyFile(quiz, destPath)
+        if err != nil {
+            fmt.Println("Error during file copying:", err)
+            os.Exit(1)
+        }
+    }
+}
--- qpm/src/cmds/utils.go
+++ qpm/src/cmds/utils.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"flag"
 )
 
 func DirExists(path string) bool {
@@ -34,4 +35,34 @@ func dirExistsWithWarningAndExit(path string) {
 		fmt.Println("Please run qpm --init first")
 		os.Exit(1)
 	}
+}
+
+func IsFlagPassed(name string) bool {
+    found := false
+    flag.Visit(func(f *flag.Flag) {
+        if f.Name == name {
+            found = true
+        }
+    })
+    return found
+}
+
+func FileExists(filename string) bool {
+    info, err := os.Stat(filename)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return !info.IsDir()
+}
+
+func CopyFile(in, out string) (int64, error) {
+   i, err := os.Open(in)
+   if err != nil { return 0, err }
+   defer i.Close()
+
+   o, err := os.Create(out)
+   if err != nil { return 0, err }
+   defer o.Close()
+
+   return o.ReadFrom(i)
 }
