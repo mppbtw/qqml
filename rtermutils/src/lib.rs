f38d8ad078cbf4f8b7c2b3a948864e9492bd1fad
mppbtw <mppdevv@skiff.com>
2023-09-06 20:12:56 +0100

    added the license comments

--- rtermutils/src/lib.rs
+++ rtermutils/src/lib.rs
@@ -1,15 +1,29 @@
+//  QQML or the Quiz Question Markup Language.
+//  Copyright (C) 2023 'MrPiggyPegasus'
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #[link(name = "termutils", kind = "static")]
 extern "C" {
     pub fn get_cursor_lines() -> i32;
     pub fn get_cursor_cols() -> i32;
     pub fn clear_screen_with_height() -> i32;
     pub fn clear_screen_with_width() -> i32;
-
     // By 'break cursor' I mean that the cursor will be in some arbitrary place
     // after calling the function. This is OK because the we might need to do
     // more computation before clearing the screen to prevent flickering.
     pub fn break_cursor_with_height() -> i32;
-
     // By 'break cursor' I mean that the cursor will be in some arbitrary place
     // after calling the function. This is OK because the we might need to do
     // more computation before clearing the screen to prevent flickering.
--- termutils/termutils.c
+++ termutils/termutils.c
@@ -1,23 +1,34 @@
+//  QQML or the Quiz Question Markup Language.
+//  Copyright (C) 2023 'MrPiggyPegasus'
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #include <stdio.h>
 #include <ctype.h>
 #include <termios.h>
-
 #define STDIN_FILENO 0
 #define SIZE 1024
 #define cursorjmp(cols, lines) printf("\033[%d;%dH", cols, lines);
-
 const char ANSI_CLEAR[] = "\033[2J";
 const char ANSI_EXIT_ALT_SCREEN[] = "\033[?1049l";
 const char ANSI_ENTER_ALT_SCREEN[] = "\033[?1049h";
 const char ANSI_GET_CURSOR_POS[] = "\033[6n";
-
 struct TerminalSize clear_screen_with_termsize();
-
 struct TerminalSize {
     int width;
     int height;
 };
-
 struct CursorPosition {
     int cols;
     int lines;
@@ -27,16 +38,13 @@ struct CursorPosition get_cursor_position() {
     // Store the initial terminal settings
     tcgetattr(STDIN_FILENO, &original);
     changed = original;
-
     // Set the new setings
     changed.c_lflag &= ~(ICANON | ECHO);
     changed.c_cc[VMIN] = 1;
     changed.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &changed);
-
     // Put the cursor pos into stdin
     printf(ANSI_GET_CURSOR_POS);
-
     // Read from that buffer
     int i = 0;
     int ch = 0;
@@ -67,19 +75,16 @@ int get_cursor_cols() {
 int get_cursor_lines() {
     return get_cursor_position().lines;
 }
-
 char read_single_char() {
     struct termios old_settings, new_settings;
     tcgetattr(STDIN_FILENO, &old_settings);
     new_settings = old_settings;
-
     new_settings.c_lflag &= (~ICANON & ~ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
     char c = getc(stdin);
     tcsetattr(STDIN_FILENO, TCSANOW, &old_settings);
     return c;
 }
-
 void enter_alt_screen() {
     printf(ANSI_ENTER_ALT_SCREEN);
     fflush(stdout);
@@ -88,7 +93,6 @@ void exit_alt_screen() {
     printf(ANSI_EXIT_ALT_SCREEN);
     fflush(stdout);
 }
-
 void hide_cursor() {
     printf("\e[?25l");
     fflush(stdout);
@@ -97,17 +101,14 @@ void show_cursor() {
     printf("\e[?25h");
     fflush(stdout);
 }
-
 int clear_screen_with_width() {
     return clear_screen_with_termsize().width;
 }
 int clear_screen_with_height() {
     return clear_screen_with_termsize().height;
 }
-
 struct TerminalSize clear_screen_with_termsize() {
     struct termios original, changed;
-
     // Change terminal settings
     tcgetattr(STDIN_FILENO, &original);
     changed = original;
@@ -115,31 +116,20 @@ struct TerminalSize clear_screen_with_termsize() {
     changed.c_cc[VMIN] = 1;
     changed.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &changed);
-
     printf(ANSI_CLEAR);
-
     // Move the cursor really far away
     cursorjmp(999, 999);
-
     struct CursorPosition pos = get_cursor_position();
-
     // Move to upper left corner
     cursorjmp(1, 1);
-
     // Restore terminal settings
     tcsetattr(STDIN_FILENO, TCSANOW, &original);
     fflush(stdout);
-
     struct TerminalSize t = {pos.cols, pos.lines};
     return t;
 }
-
-// By 'break cursor' I mean that the cursor will be in some arbitrary place
-// after calling the function. This is OK because the we might need to do
-// more computation before clearing the screen to prevent flickering.
 struct TerminalSize break_cursor_with_termsize() {
     struct termios original, changed;
-
     // Change terminal settings
     tcgetattr(STDIN_FILENO, &original);
     changed = original;
@@ -147,33 +137,26 @@ struct TerminalSize break_cursor_with_termsize() {
     changed.c_cc[VMIN] = 1;
     changed.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &changed);
-
     // Move the cursor really far away where it will stay
     cursorjmp(999, 999);
-
     struct CursorPosition pos = get_cursor_position();
-
     // Restore terminal settings
     tcsetattr(STDIN_FILENO, TCSANOW, &original);
     fflush(stdout);
-
     struct TerminalSize t = {pos.cols, pos.lines};
     return t;
 }
-
 int break_cursor_with_width() {
     return break_cursor_with_termsize().width;
 }
 int break_cursor_with_height() {
     return break_cursor_with_termsize().height;
 }
-
 void clear_screen() {
     printf(ANSI_CLEAR);
     cursorjmp(1, 1);
     fflush(stdout);
 }
-
 void close_stdin() {
     fclose(stdin);
 }
