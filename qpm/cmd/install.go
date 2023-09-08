24dd5a01e90cc20490d8b0221a49e0684e2a6ad8
mppbtw <mppdevv@skiff.com>
2023-09-08 22:01:15 +0100

    added install things which come to think of it is pretty much the purpose of a package manager so in that case i guess im done cya next year bozos

--- qpm/cmd/install.go
+++ qpm/cmd/install.go
@@ -1,10 +1,28 @@
+//  QQML or the Quiz Question Markup Language.
+//  Copyright (C) 2023 'MrPiggyPegasus'
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"qpm/internal"
 	"strings"
+	"unicode"
 
 	"github.com/spf13/cobra"
 )
@@ -26,8 +44,6 @@ var (
 				os.Exit(1)
 			}
 
-			pathSegments := strings.Split(filePath, "/")
-			fileName := pathSegments[len(pathSegments)-1]
 			homeDir, err := os.UserHomeDir()
 			if err != nil {
 				fmt.Println("Failed to get your home directory:", err.Error())
@@ -40,10 +56,54 @@ var (
 				fmt.Println("Failed to read the file", filePath+":", err.Error())
 			}
 
-			os.WriteFile(qpmDir+fileName, srcContents, os.FileMode(0644))
+			var name string
+			for {
+				name = ""
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print("Please enter the name of the quiz: ")
+				name, err = reader.ReadString('\n')
+				name = strings.TrimSuffix(name, "\n")
+				if err != nil {
+					fmt.Println("Failed to read from stdin")
+					os.Exit(1)
+				}
+
+				// Validate the name
+				if strings.Contains(name, "/") || strings.Contains(name, "\\") {
+					fmt.Printf("The name must contain no slashes\n\n")
+					continue
+				}
+				if len(name) == 0 {
+					fmt.Printf("The name must not be empty\n\n")
+					continue
+				}
+				if internal.PathExists(qpmDir + "local/src/" + name + ".qqml") {
+					fmt.Printf("A quiz of that name already exists\n\n")
+					continue
+				}
+
+				isAscii := true
+				for _, ch := range name {
+					if ch > unicode.MaxASCII {
+						isAscii = false
+					}
+				}
+
+				if !isAscii {
+					fmt.Printf("Only ASCII characters are allowed in the name\n\n")
+					continue
+				}
+				break
+			}
+
+			if err := os.WriteFile(qpmDir+"local/src/"+name+".qqml", srcContents, os.FileMode(0644)); err != nil {
+				fmt.Println("Failed to install quiz:", err.Error())
+				os.Exit(1)
+			}
 		},
 	}
 )
+
 func init() {
 	installCmd.Flags().StringVarP(&filePath, "file", "f", "", "install from local file")
 	installCmd.MarkFlagFilename("file")
