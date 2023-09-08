7013a5dd1a000ecd1528c4126d9e29f16f819557
mppbtw <mppdevv@skiff.com>
2023-09-08 14:31:13 +0100

    i think this should me on main

--- qpm/cmd/init.go
+++ qpm/cmd/init.go
@@ -18,18 +18,46 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"os"
 	"qpm/internal"
+
+	"github.com/spf13/cobra"
 )
 
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "Initialise the ~/.qpm directory.",
+	Short: "Initialise the ~/.qpm directory",
 	Long:  "Initialise the ~/.qpm directory to store QQML quiz files",
 	Run: func(cmd *cobra.Command, args []string) {
-		if internal.IsInitialised() {
+		res, err := internal.IsInitialised()
+		if err != nil {
+			fmt.Println("Failed to tell if QPM is already initialised:", err.Error())
+			os.Exit(1)
+		}
+		if res {
 			fmt.Println("QPM is already initialised.")
-			return
+			os.Exit(0)
+		}
+
+		requiredDirs := [5]string{"", "local", "local/src", "local/log", "repos"}
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("Failed to get your home directory:", err.Error())
+			os.Exit(1)
+		}
+		qpmDir := homeDir + "/.qpm/"
+		for _, dir := range requiredDirs {
+			dir = qpmDir + dir
+			if internal.DirExists(dir) {
+				fmt.Println("The directory", dir, "already exists")
+				continue
+			}
+			err := os.Mkdir(dir, os.FileMode(0777))
+			if err != nil {
+				fmt.Println("Failed to create the directory", dir+":", err.Error())
+				os.Exit(1)
+			}
+			fmt.Println("Successfully created the directory", dir)
 		}
 	},
 }
