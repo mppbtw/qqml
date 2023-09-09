554495ba3d4fca63e98468ea1d9a958d9e49b253
mppbtw <mppdevv@skiff.com>
2023-09-09 16:35:57 +0100

    added run command

---
+++ qpm/cmd/run.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+    "fmt"
+
+    "github.com/spf13/cobra"
+)
+
+var (
+    fromFilePath string
+    runCmd = &cobra.Command{
+        Use: "run",
+        Short: "Run an installed quiz",
+        Long: "Run an installed quiz from any of the available repos or locally installed files",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("placeholder")
+        },
+    }
+)
+
+func init() {
+    runCmd.Flags().StringVarP(&fromFilePath, "f", "file", "", "from a file")
+    rootCmd.AddCommand(runCmd)
+}
