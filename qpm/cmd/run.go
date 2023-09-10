1621aa4578c8887ff609dfcc4574527acd9346b8
mppbtw <mppdevv@skiff.com>
2023-09-10 15:19:35 +0100

    Revert "Revert "the mascot should be a quoll i think""
    This reverts commit bea252852dbdf988935cd9ab09083eadeeaeb11b.

--- qpm/cmd/run.go
+++ qpm/cmd/run.go
@@ -1,24 +1,24 @@
 package cmd
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var (
-    fromFilePath string
-    runCmd = &cobra.Command{
-        Use: "run",
-        Short: "Run an installed quiz",
-        Long: "Run an installed quiz from any of the available repos or locally installed files",
-        Run: func(cmd *cobra.Command, args []string) {
-            fmt.Println("placeholder")
-        },
-    }
+	fromFilePath string
+	runCmd       = &cobra.Command{
+		Use:   "run",
+		Short: "Run an installed quiz",
+		Long:  "Run an installed quiz from any of the available repos or locally installed files",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("placeholder")
+		},
+	}
 )
 
 func init() {
-    runCmd.Flags().StringVarP(&fromFilePath, "f", "file", "", "from a file")
-    rootCmd.AddCommand(runCmd)
+	runCmd.Flags().StringVarP(&fromFilePath, "file", "f", "", "from a file")
+	rootCmd.AddCommand(runCmd)
 }
