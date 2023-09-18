63bc0dec838aa4b67443131aef66482e5b132c9e
mppbtw <mppdevv@skiff.com>
2023-09-18 08:42:18 +0100

    im not sure what this commit is but lets do it anyway cause im back

--- qpm/internal/locate/locate.go
+++ qpm/internal/locate/locate.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"qpm/internal"
+	"strings"
 )
 
 type ErrNotInitialised struct{}
@@ -18,7 +19,14 @@ func (e *ErrNoSuchCacheFile) Error() string {
 	return "The cache file does not exist"
 }
 
+type ErrNoSuchLogFile struct{}
+
+func (e *ErrNoSuchLogFile) Error() string {
+	return "The cache file does not exist"
+}
+
 func FindCacheFile(name string) ([]string, error) {
+	name = name + ".qqml.json"
 	filesFound := []string{}
 
 	res, err := internal.IsInitialised()
@@ -49,5 +57,52 @@ func FindCacheFile(name string) ([]string, error) {
 		}
 	}
 
+	if len(filesFound) == 0 {
+		return []string{}, &ErrNoSuchCacheFile{}
+	}
 	return filesFound, nil
+}
+
+func FindLogFile(name string) ([]string, error) {
+	name = name + ".qqml.json"
+	filesFound := []string{}
+
+	res, err := internal.IsInitialised()
+	if err != nil {
+		return nil, err
+	}
+	if !res {
+		return nil, &ErrNotInitialised{}
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	qpmDir := homeDir + "/.qpm/"
+
+	// Search local directory first
+
+	localLogDir := qpmDir + "local/cache/"
+	localLogFiles, err := os.ReadDir(localLogDir)
+	if err != nil {
+		fmt.Println("Failed to read the directory", localLogDir+":"+err.Error())
+	}
+	for _, f := range localLogFiles {
+		if !f.IsDir() || f.Name() == name {
+			filesFound = append(filesFound, localLogDir+name)
+			break
+		}
+	}
+
+	if len(filesFound) == 0 {
+		return []string{}, &ErrNoSuchLogFile{}
+	}
+	return filesFound, nil
+}
+
+func GenLogFileFromCache(cachePath string) string {
+	pathSegments := strings.Split(cachePath, "/")
+	pathSegments[len(pathSegments)-2] = "cache"
+	return strings.Join(pathSegments, "/")
 }
