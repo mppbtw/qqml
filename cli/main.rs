f9d5ddf10cea8dd4080b83f9659d43234234d944
MrPiggyPegasus <charbalone@gmail.com>
2023-09-21 00:36:16 +0100

    added some argparse stuff dunno if itll work or no

--- cli/main.rs
+++ cli/main.rs
@@ -1,12 +1,21 @@
 use std::convert::Infallible;
 use std::process::exit;
 
+mod cmd;
 mod argparse;
 
- fn main() -> Infallible {
-    println!("running cli command parsing");
-    let rootCmd = argparse::Command {
+use argparse::CommandBuilder;
+use argparse::Command;
 
-    };
+fn main() -> Infallible {
+    println!("running cli command parsing");
+    let root_cmd = Command::new(CommandBuilder {
+        short: "wad",
+        long: "adawdaw",
+        ..Default::default()
+    });
+
+    root_cmd.execute();
+
     exit(0)
 }
