bcfecfac57bea6c54572c4fe42893c0c474d7af8
MrPiggyPegasus <charbalone@gmail.com>
2023-09-21 13:22:24 +0100

    finished some things

--- cli/argparse.rs
+++ cli/argparse.rs
@@ -1,4 +1,7 @@
-use std::{convert::Infallible, process::exit};
+use std::convert::Infallible;
+use std::env::args;
+use std::process::exit;
+use std::sync::Arc;
 
 /// This is a pretty simple argparsing solution inspired by Go's Cobra library.
 pub struct Command {
@@ -6,7 +9,7 @@ pub struct Command {
     usage:    &'static str,
     long:     &'static str,
     short:    &'static str,
-    run:      Option<fn()>,
+    run:      Option<fn(&[String]) -> Infallible>,
 }
 
 impl Command {
@@ -24,8 +27,42 @@ impl Command {
         self.children.push(cmd);
     }
 
+    pub fn help_screen(&self) -> Infallible {
+        println!("placeholder help screen");
+        exit(0);
+    }
+
+    fn lookup_command(&self, arg: &str) -> Option<&Command> {
+        for child in self.children.iter() {
+            if child.usage == arg {
+                return Some(&child)
+            }
+        }
+        None
+    }
+
     /// Call this on the root command to initate the parsing sequence.
-    pub fn execute(&self) -> Infallible {
+    pub fn execute<A: Into<Arc<[String]>>>(&self, args: A) -> Infallible {
+        let args: &[String] = &args.into();
+
+        // If it's a leaf command
+        if self.run.is_some() {
+            self.run.unwrap()(&args[..]);
+        // It has subcommands
+        } else {
+            match args.get(0) {
+                Some(arg) => {
+                    if let Some(c) = self.lookup_command(arg) {
+                        c.execute(&args[1..])
+                    } else {
+                        println!("Unknown command: {}", arg);
+                        exit(1);
+                    };
+                },
+                None => {self.help_screen();},
+            }
+        }
+
         exit(0)
     }
 }
@@ -35,5 +72,5 @@ pub struct CommandBuilder {
     pub usage: &'static str,
     pub long:  &'static str,
     pub short: &'static str,
-    pub run:   Option<fn()>,
+    pub run:   Option<fn(&[String]) -> Infallible>,
 }
--- cli/main.rs
+++ cli/main.rs
@@ -1,21 +1,26 @@
 use std::convert::Infallible;
+use std::env::args;
 use std::process::exit;
+use std::sync::Arc;
 
-mod cmd;
 mod argparse;
+mod cmd;
 
-use argparse::CommandBuilder;
 use argparse::Command;
+use argparse::CommandBuilder;
 
 fn main() -> Infallible {
     println!("running cli command parsing");
-    let root_cmd = Command::new(CommandBuilder {
-        short: "wad",
-        long: "adawdaw",
-        ..Default::default()
+    let mut root_cmd = Command::new(CommandBuilder {
+        short: "The QQML interpreter",
+        long:  "The QQML interpreter.",
+        run:   None,
+        usage: "",
     });
 
-    root_cmd.execute();
+    cmd::init(&mut root_cmd);
+    let args = args().collect::<Arc<[String]>>();
+    root_cmd.execute(&args[1..]);
 
     exit(0)
 }
