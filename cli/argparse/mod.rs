5b81dc7b5e3bc947a2595e9360db54cdd0081150
mppbtw <mppdevv@skiff.com>
2023-10-02 12:52:30 +0100

    aint no body gonna tell me what my commit message gon be

--- cli/argparse/mod.rs
+++ cli/argparse/mod.rs
@@ -17,6 +17,10 @@
 use std::convert::Infallible;
 use std::process::exit;
 
+mod flag;
+
+pub use flag::*;
+
 /// This is a pretty simple argparsing solution inspired by Go's Cobra library.
 pub struct Command {
     children: Vec<Command>,
@@ -201,79 +205,4 @@ pub struct CommandBuilder {
     pub run:   Option<fn(&[String], AnsweredFlags) -> Infallible>,
     pub args:  usize,
     pub flags: Vec<Flag>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
-pub struct Flag {
-    pub long:    &'static str,
-    pub aliases: Vec<&'static str>,
-    pub arg:     Option<FlagArgumentType>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum FlagArgumentType {
-    Int,
-    Uint,
-    String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
-pub struct AnsweredFlag {
-    pub long: &'static str,
-    pub arg:  Option<AnsweredFlagArgument>,
-}
-impl AnsweredFlag {
-    pub fn int(&self) -> Result<&isize, ErrIncorrectArgType> {
-        self.arg.as_ref().unwrap().int()
-    }
-
-    pub fn uint(&self) -> Result<&usize, ErrIncorrectArgType> {
-        self.arg.as_ref().unwrap().uint()
-    }
-
-    pub fn string(&self) -> Result<&String, ErrIncorrectArgType> {
-        self.arg.as_ref().unwrap().string()
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum AnsweredFlagArgument {
-    Int(isize),
-    Uint(usize),
-    String(String),
-}
-impl AnsweredFlagArgument {
-    pub fn int(&self) -> Result<&isize, ErrIncorrectArgType> {
-        match self {
-            Self::Int(i) => Ok(i),
-            _ => Err(ErrIncorrectArgType),
-        }
-    }
-
-    pub fn uint(&self) -> Result<&usize, ErrIncorrectArgType> {
-        match self {
-            Self::Uint(u) => Ok(u),
-            _ => Err(ErrIncorrectArgType),
-        }
-    }
-
-    pub fn string(&self) -> Result<&String, ErrIncorrectArgType> {
-        match self {
-            Self::String(s) => Ok(s),
-            _ => Err(ErrIncorrectArgType),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ErrIncorrectArgType;
-
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
-pub struct AnsweredFlags {
-    pub flags: Vec<AnsweredFlag>,
-}
-impl AnsweredFlags {
-    pub fn get(&self, name: &str) -> Option<&AnsweredFlag> {
-        self.flags.iter().find(|f| f.long == name)
-    }
 }
