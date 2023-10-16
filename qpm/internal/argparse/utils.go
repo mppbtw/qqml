48546fdeb4fc7fb520cd114cc94e94e29ed94dc7
MrPiggyPegasus <charbalone@gmail.com>
2023-10-16 11:46:14 +0100

    where is s1mple

--- qpm/internal/argparse/utils.go
+++ qpm/internal/argparse/utils.go
@@ -62,3 +62,48 @@ func separateLines(inp [][]string, gapSize int8) string {
 	}
 	return strings.Join(lines, "\n")
 }
+
+func merge(a []int, b []int) []int {
+    final := []int{}
+    i := 0
+    j := 0
+    for i < len(a) && j < len(b) {
+        if a[i] < b[j] {
+            final = append(final, a[i])
+            i++
+        } else {
+            final = append(final, b[j])
+            j++
+        }
+    }
+    for ; i < len(a); i++ {
+        final = append(final, a[i])
+    }
+    for ; j < len(b); j++ {
+        final = append(final, b[j])
+    }
+    return final
+}
+
+func mergeSort(items []int) []int {
+    if len(items) < 2 {
+        return items
+    }
+    first := mergeSort(items[:len(items)/2])
+    second := mergeSort(items[len(items)/2:])
+    return merge(first, second)
+}
+
+
+func reverseArr(inp []int) []int {
+	for i:=0; i < len(inp)/2; i++ {
+		tmp := inp[i]
+		inp[i] = inp[len(inp)-1]
+		inp[len(inp)-1] = tmp
+	}
+	return inp
+}
+
+func backwardsSort(inp []int) []int {
+	return reverseArr(mergeSort(inp))
+}
