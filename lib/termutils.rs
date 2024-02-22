0b305df3a34581612948754314ddc4dd2cb43418
mppdevv <mppdevv@skiff.com>
2024-02-22 13:04:38 +0000

    Merged termutils into the main crate

--- lib/termutils.rs
+++ lib/termutils.rs
@@ -16,14 +16,9 @@
 
 #[link(name = "termutils", kind = "static")]
 extern "C" {
-    pub fn get_cursor_lines() -> i32;
-    pub fn get_cursor_cols() -> i32;
     pub fn clear_screen_with_height() -> i32;
     pub fn clear_screen_with_width() -> i32;
-    // By 'break cursor' I mean that the cursor will be in some arbitrary place
-    // after calling the function. This is OK because the we might need to do
-    // more computation before clearing the screen to prevent flickering.
-    pub fn break_cursor_with_height() -> i32;
+
     // By 'break cursor' I mean that the cursor will be in some arbitrary place
     // after calling the function. This is OK because the we might need to do
     // more computation before clearing the screen to prevent flickering.
@@ -34,5 +29,4 @@ extern "C" {
     pub fn hide_cursor();
     pub fn show_cursor();
     pub fn clear_screen();
-    pub fn close_stdin();
 }
